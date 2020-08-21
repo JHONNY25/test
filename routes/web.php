cf29a6d670805c7a94ff365324fd55c5aea3ef24
JHONNY25 <37971335+JHONNY25@users.noreply.github.com>
2020-08-21 10:42:06 -0600

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','UserController@index')->name('user.index');
+Route::post('/update-status','UserController@updateStatus')->name('user.updateStatus');
+
