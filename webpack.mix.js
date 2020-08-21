98a590898594bcaa9ac9f319c791eae984fb3436
JHONNY25 <37971335+JHONNY25@users.noreply.github.com>
2020-08-20 22:53:50 -0600

    update for use vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js','resources/js/app.js')
+mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
