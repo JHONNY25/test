65f6be40b11c99703c008dc243e25f5c735fa357
JHONNY25 <37971335+JHONNY25@users.noreply.github.com>
2020-08-21 17:28:44 -0600

    add new route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/','UserController@index')->name('user.index');
+Route::get('/get-users','UserController@getUsersByStatus')->name('user.getUsersByStatus');
 Route::post('/update-status','UserController@updateStatus')->name('user.updateStatus');
 
