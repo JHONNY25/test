9ddc0158ba2dc078e1f172c68c136518cb635bc2
JHONNY25 <37971335+JHONNY25@users.noreply.github.com>
2020-09-02 11:02:21 -0600

    example

--- resources/js/components/TableComponent.vue
+++ resources/js/components/TableComponent.vue
@@ -1,44 +1,43 @@
 <template>
-    <div>
-        <table class="table table-striped table-dark">
-            <thead >
-                <tr>
-                    <th scope="col" v-for="item in headers" :key="item">{{ item }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(item,index) in data" :key="index">
-                    <th scope="row">{{ item.id }}</th>
-                    <td>{{ item.name }}</td>
-                    <td>{{ item.email }}</td>
-                    <td>{{ item.status === 1 ? 'Activo' : 'Inactivo' }}</td>
-                    <td>
-                        <div class="custom-control custom-switch">
-                            <input v-model="status" type="checkbox" class="custom-control-input" id="customSwitch1">
-                            <label class="custom-control-label" for="customSwitch1">Inactivo/Activo</label>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+<div>
+    <table class="table table-striped table-dark">
+        <thead>
+            <tr>
+                <th scope="col" v-for="item in headers" :key="item">{{ item }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item,index) in data" :key="index">
+                <th scope="row">{{ item.id }}</th>
+                <td>{{ item.name }}</td>
+                <td>{{ item.email }}</td>
+                <td>{{ item.status === 1 ? 'Activo' : 'Inactivo' }}</td>
+                <td>
+                    <div class="custom-control custom-switch">
+                        <input v-model="status" type="checkbox" class="custom-control-input" id="customSwitch1">
+                        <label class="custom-control-label" for="customSwitch1">Inactivo/Activo</label>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                status: 0
-            }
-        },
-        props:['data','headers'],
-        methods: {
-            changeStatus(item){
-                let users = await axios.post('/update-status',item)
+export default {
+    data() {
+        return {
+            status: 0
+        }
+    },
+    props: ['data', 'headers'],
+    methods: {
+        changeStatus(item) {
+            let users = await axios.post('/update-status', item)
 
-                this.users = await users.data
-            },
-            }
+            this.users = await users.data
         },
     }
+}
 </script>
